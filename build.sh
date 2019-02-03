e24cc7cf07d8fc3fd358af9be61185b9db548651
William Squires <wwwil.squires@gmail.com>
2019-02-03 02:39:37 +0000

    Add URL for image file and change script to accept 7z format

--- build.sh
+++ build.sh
@@ -8,12 +8,13 @@ echo "BUILD - Will now pull Docker image"
 docker pull edwardotme/raspbian-customiser:v0.2
 
 echo "FETCH - Will now fetch source image"
-IMAGE_LINK=
+IMAGE_LINK=https://www.qlcplus.org/downloads/raspberry/713c4e9fbc9d61d8508f6533d97e062e/qlcplus_raspbian_stretch_20181110.7z
 wget -nv $IMAGE_LINK
-IMAGE_ZIP=$(basename $IMAGE_LINK)
-unzip -o $IMAGE_ZIP
-rm $IMAGE_ZIP
-IMAGE_IMG=${IMAGE_ZIP%.zip}.img
+IMAGE_7Z=$(basename $IMAGE_LINK)
+apt-get -y install p7zip-full
+7z e $IMAGE_7Z
+rm $IMAGE_7Z
+IMAGE_IMG=${IMAGE_ZIP%.7z}.img
 echo "FETCH - Successfully fetched $IMAGE_IMG"
 
 echo "INSTALL - Will now install modifications"
